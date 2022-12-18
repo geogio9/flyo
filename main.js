72ee3af53affcb8606f0738c7487cc5c4d1749a3
geogio9 <12345678gvianidze@gmail.com>
2022-12-18 23:43:47 +0400

    j

--- main.js
+++ main.js
@@ -4,27 +4,24 @@ const error = document.querySelector(".error")
 const todaySearch = document.querySelector(".today-search")
 const todayButton = document.querySelector(".today-button")
 const todayError = document.querySelector(".today-error")
-const re =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-button.addEventListener("click",function(){
-    if( re.test(search.value)){ 
+button.addEventListener("click", function () {
+    if (re.test(search.value)) {
+        search.style.border = "0.625px solid #07043B"
         error.style.display = "none"
-    }
-    else{
+    } else {
         search.style.border = "0.625px solid #EF4877"
         error.style.display = "block"
     }
 })
 
-todayButton.addEventListener("click",function(){
-    if( re.test(todaySearch.value)){ 
+todayButton.addEventListener("click", function () {
+    if (re.test(todaySearch.value)) {
+        todaySearch.style.border = "0.625px solid #07043B"
         todayError.style.display = "none"
-    }
-    else{
+    } else {
         todaySearch.style.border = "0.625px solid #EF4877"
         todayError.style.display = "block"
     }
 })
-
-
-
